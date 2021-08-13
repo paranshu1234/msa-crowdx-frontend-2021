0bcfe10a03989e2067363b48111835ada93a069e
paranshu1234 <paranshu1234@gmail.com>
2021-08-13 10:21:13 +0530

    Added routing functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,30 @@ import React from 'react';
 import { loadTheme } from '@fluentui/react';
 import { appTheme } from './ThemeDefinitions';
 import AppBar from './Components/AppBar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 loadTheme(appTheme);
 
 function App() {
   return (
     <div className="App">
-      <AppBar />
+       <Router>
+       <AppBar />
+          <Switch>
+            <Route path="/login">
+              <Home />
+            </Route>
+            <Route path="/signup">
+              <About />
+            </Route>
+          </Switch>
+       </Router>
     </div>
   );
 }
