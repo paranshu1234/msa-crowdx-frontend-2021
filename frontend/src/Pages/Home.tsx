39fe3174fef1daf89d8a85b845163c03ddeffdde
paranshu1234 <paranshu1234@gmail.com>
2021-08-14 12:21:24 +0530

    Added apollo client

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useGetProjects } from '../hooks/projects/useGetProjects'
 
 const Home: React.FC = () => {
+    const projects = useGetProjects();
+    console.log(projects)
+
+
     return (
         <div>
-            <h1>Home Page</h1>   
+            <h1>Home</h1>
         </div>
     )
 }
