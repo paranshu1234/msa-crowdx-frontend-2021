19d168cc1bedcd03982951cb53248617a1903368
paranshu1234 <paranshu1234@gmail.com>
2021-09-12 13:33:01 +0530

    Added base home pae ui

--- frontend/src/Components/AppBar.tsx
+++ frontend/src/Components/AppBar.tsx
@@ -11,11 +11,21 @@ import {
 const AppBar: React.FC = () => {
     return (
        <header className="App-header">
-            <Link to="/"><img  src={logo} alt="Crowdx" /></Link>
+           <div className="left-content">
+                <Link to="/" className="logo-link"><img  src={logo} alt="Crowdx" /></Link>
+                <div className="navlink-wrapper">
+                    <p>Product</p>
+                    <p>For creators</p>
+                    <p>Pricing</p>
+                    <p>Resources</p>
+                    <p>Starter kits</p>
+                </div>
+           </div>
+         
             <div className="button-group-wrapper">
-                <SearchBox placeholder="Search a Creator" onSearch={newValue => console.log('value is ' + newValue)} />
-                <Link to="/login"><DefaultButton text="Login" /></Link>
-                <Link to="/signup"><PrimaryButton text="Get Started"/></Link>
+                <SearchBox placeholder="Search a Creator" onSearch={newValue => console.log('value is ' + newValue)} className="item" />
+                <Link to="/login" className="item"><DefaultButton text="Login" /></Link>
+                <Link to="/signup" className="item"><PrimaryButton text="Get Started"/></Link>
             </div>
        </header>
     );
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useGetProjects } from '../hooks/projects/useGetProjects'
 import { IIconProps } from '@fluentui/react';
-import { IconButton } from '@fluentui/react/lib/Button';
+import { DefaultButton, IconButton, PrimaryButton } from '@fluentui/react/lib/Button';
+import {
+    Link
+  } from "react-router-dom";
+  
 import "./Home.css"
 
 const twitterIcon: IIconProps = { iconName: 'twitter' };
@@ -17,7 +21,37 @@ const Home: React.FC = () => {
     return (
         <div className="home-page-wrapper">
 
-            <h1>Home</h1>
+            <section className="hero">
+                <div className="hero-text-wrapper">
+                    <div className="hero-header-wrapper">
+                        <h1>Change the way art is valued</h1>
+                         <p>Let your most passionate fans support your creative work via monthly membership</p>
+                    </div>
+                    <Link to="/signup" ><DefaultButton text="Get Started" className="hero-btn"/></Link>
+                </div>
+
+                <div className="hero-gif-wrapper">
+                    <section className="hero-gif"></section>
+                </div>
+            </section>
+            <section className="search-creators">
+                <h1>Search the 100,000+ creators on Crowdx</h1>
+                <Link to="/creator" ><PrimaryButton text="Find a creator you love" className="find-creator-btn"/></Link>
+            </section>
+            <section className="crowdx-introduction">
+                <div className="intro-contentwrapper">
+                     <h1>What's Crowdx?</h1>
+                    <p>On Patreon, you can let your fans become active participants in the
+                         work they love by offering them a monthly membership. You give them access to exclusive content,
+                         community and insight into your creative process. 
+                         In exchange, you get the freedom to do your best work and the stability you need 
+                         to build an independent creative career.</p>
+                </div>         
+            </section>
+            <section className="cta-bottom">
+                <h1>Are you ready to take back control?</h1>
+                <Link to="/signup" ><PrimaryButton text="Get started" className="cta-btn"/></Link>
+            </section>
             <footer>
                 <div className="footer-logo-wrapper">
                     <h1>Crowdx</h1>
