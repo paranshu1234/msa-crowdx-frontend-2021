fdaac6dfbea9b932f058f879c94056e77f55f561
paranshu1234 <paranshu1234@gmail.com>
2021-09-23 13:36:55 +0530

    Added-post-card

--- frontend/src/Components/AppBar.tsx
+++ frontend/src/Components/AppBar.tsx
@@ -3,11 +3,21 @@ import { SearchBox } from '@fluentui/react/lib/SearchBox';
 import logo from '../logo.svg';
 import "./AppBar.css"
 import { DefaultButton, PrimaryButton } from '@fluentui/react';
+import { Icon } from '@fluentui/react/lib/Icon';
 import {
     Link
   } from "react-router-dom";
+
+  import { IPersonaSharedProps, Persona, PersonaSize, PersonaPresence } from '@fluentui/react/lib/Persona';
   
 import  { useLocation} from "react-router-dom";
+
+
+const navPersona: IPersonaSharedProps = {
+    imageUrl: 'https://lh3.googleusercontent.com/ogw/ADea4I4m4w-wrbRDy9TnlII-tgvmmTlRA64wqYHe_MklEQ=s128-c-mo',
+    imageInitials: 'PM',
+  };
+
 
 const AppBar: React.FC = (props) => {
     const location = useLocation();
@@ -15,29 +25,50 @@ const AppBar: React.FC = (props) => {
     return (
        <header className="App-header">
            <div className="left-content">
-                <Link to="/" className="logo-link"><img  src={logo} alt="Crowdx" /></Link>
-                <div className="navlink-wrapper">
-                    <p className="nav-item">Product</p>
-                    <p className="nav-item">For creators</p>
-                    <p className="nav-item">Pricing</p>
-                    <p className="nav-item">Resources</p>
-                    <p className="nav-item">Starter kits</p>
-                </div>
-           </div>
+                <Link to="/" className="logo-link"><img className="logo-image" src={logo} alt="Crowdx" /></Link>
                 {
-                    location.pathname === `/login` || location.pathname === `/signup` ? (
-                     null
-             
-                ) : ( 
-                     <div className="button-group-wrapper">
-                        <SearchBox placeholder="Search a Creator" onSearch={newValue => console.log('value is ' + newValue)} className="item" />
-                        <Link to="/login" className="item"><DefaultButton text="Login" /></Link>
-                        <Link to="/signup" className="item"><PrimaryButton text="Get Started"/></Link>    
-                
-                    </div>
+                    location.pathname === `/dashboard` ? 
+                    (
+                        null     
+                    ) : (
+                        <div className="navlink-wrapper">
+                            <p className="nav-item">Product</p>
+                            <p className="nav-item">For creators</p>
+                            <p className="nav-item">Pricing</p>
+                            <p className="nav-item">Resources</p>
+                            <p className="nav-item">Starter kits</p>
+                         </div>
                     )
                 }
-              
+               
+           </div>
+                {
+                    location.pathname === `/dashboard` ? (
+                        <div className="button-group-wrapper">
+                            <SearchBox placeholder="Search a Creator" onSearch={newValue => console.log('value is ' + newValue)} className="item-auth" />
+                            <Icon iconName="Message" className="button-group-icn item-auth" />
+                            <Persona
+                            {...navPersona}
+                            size={PersonaSize.size32}
+                            presence={PersonaPresence.none}
+                            hidePersonaDetails={true}
+                            imageAlt="Creator-card-img"
+                            className="item-auth"
+                             />
+                        </div>
+                    ) : (
+                        location.pathname === `/login` || location.pathname === `/signup` ? (
+                            null
+                    
+                       ) : ( 
+                            <div className="button-group-wrapper">
+                               <SearchBox placeholder="Search a Creator" onSearch={newValue => console.log('value is ' + newValue)} className="item" />
+                               <Link to="/login" className="item"><DefaultButton text="Login" /></Link>
+                               <Link to="/signup" className="item"><PrimaryButton text="Get Started"/></Link>    
+                           </div>
+                           )
+                    )
+                }
            
        </header>
     );
--- frontend/src/Components/PostCard.tsx
+++ frontend/src/Components/PostCard.tsx
@@ -1,22 +1,43 @@
-import React from 'react'
-import { DefaultEffects } from '@fluentui/react';
-import {  PrimaryButton } from '@fluentui/react/lib/Button';
-import { Separator } from '@fluentui/react/lib/Separator';
-import "./PostCard.css"
+import React from "react";
+import { DefaultEffects } from "@fluentui/react";
+import { DefaultButton, PrimaryButton } from "@fluentui/react/lib/Button";
+import { Separator } from "@fluentui/react/lib/Separator";
+import { Icon } from "@fluentui/react/lib/Icon";
+import "./PostCard.css";
 
 const PostCard: React.FC = () => {
-    return (
-        <div className="post-card" style={{ boxShadow: DefaultEffects.elevation8 }} >
-            <Separator />
-            <div className="amount-wrapper">
-                <h2 className="amount">$8</h2>
-                <p>per month</p>
-            </div>
-            <div>
-            <PrimaryButton text="Become a Supporter"  allowDisabledFocus />
-            </div>
+  return (
+    <div className="post-card" style={{ boxShadow: DefaultEffects.elevation8 }}>
+      <div className="post-card-img-wrapper">
+        <div className="unlock-content-wrapper">
+          <Icon iconName="LockSolid" className="unlock-content-icn" />
+          <h3>Unlock this post by becoming a supporter</h3>
+          <DefaultButton text="Join now" allowDisabledFocus />
         </div>
-    )
-}
+        <img
+          src="https://image.mux.com/HZ43dz01OQEzQuKC3qjfvYuKhjprX3cH02PYZeWY4dKGs/thumbnail.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5CY3o3Sk5RcUNmdDdWcmo5MWhra2lEY3Vyc2xtRGNmSU1oSFUzallZMDI0In0.eyJzdWIiOiJIWjQzZHowMU9RRXpRdUtDM3FqZnZZdUtoanByWDNjSDAyUFlaZVdZNGRLR3MiLCJleHAiOjE2MzQ5OTQ0MDcsImF1ZCI6InQiLCJ0aW1lIjoxNzAuMjA4fQ.SU8w3FjAx7AFwwrQIV4oW6B2PyZ3-EpRczV6Q1WlOLdlFI2-cuwkZp1H3xk7vYEk0nXKGcSbUHXkiP3LpgGdnqPv9Jk-2MLc-uxO_1wnLRwrs8bCIpi4gD2ceV3so5Xuv5g_S7DMs7HUY8U8ZUbJg9tTXT-zX5uEmH0icXIrgadXSkwfyQ7sYkevLfwUBNkYylUxtIdDN7k-9OQTKoU_y1PO_Eny6nnSVcPQ7zpN2Lp6BfLnrr7xGdaE5wntvAoGg80gYXi-44EOdYszv7Zczu3hrWaERoeWvR0no5PY3LwQjEGt6JahjjpQdQqyE6Ml5cVGqiSTpxRB41POjWp7oA"
+          alt="post"
+        />
+      </div>
+      <div className="post-card-content-top-wrapper">
+        <div className="time-stamp-wrapper">
+          <small>21 SEPT 2021 AT 09:36</small>
+        </div>
+        <div className="locked-svg-wrapper">
+          <Icon iconName="Lock" className="locked-svg-icn" />
+          <small>locked</small>
+        </div>
+      </div>
+      <div className="post-card-text">
+        <h2>Grown Men In Austin Powers Themed Apparel Rap Eminem Lyrics</h2>
+      </div>
+      <div className="post-card-content-bottom-wrapper">
+        <Separator />
+        <small>20 Comments</small>
+        <small>141 Likes</small>
+      </div>
+    </div>
+  );
+};
 
-export default PostCard
+export default PostCard;
